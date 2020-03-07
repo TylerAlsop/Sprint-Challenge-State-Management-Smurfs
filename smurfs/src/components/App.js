d81c7233eedc85b40139ec23b701bf9fb16eaf4a
Tyler Alsop <tylerjalsop@gmail.com>
2020-03-06 18:10:00 -0700

    Fixed the provider name.

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -10,14 +10,14 @@ import SmurfList from './SmurfList';
 class App extends Component {
   render() {
     return (
-      <CartContext.Provider value={{ }}>
+      <SmurfContext.Provider value={{ }}>
         <div className="App">
           <h1>SMURFS! 2.0 W/ Redux</h1>
           <div>Welcome to your state management version of Smurfs!</div>
           <div>Start inside of your `src/index.js` file!</div>
           <div>Have fun!</div>
         </div>
-      </CartContext.Provider>
+      </SmurfContext.Provider>
 
     );
   }
