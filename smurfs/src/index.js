d28e67ab23a38687215e968f6eb45cf5482ae2d0
Tyler Alsop <tylerjalsop@gmail.com>
2020-03-06 17:45:17 -0700

    Added and commented out the data rendered on the localhoust:333/smurfs in index.js so that I can reference it later if needed.

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -4,3 +4,7 @@ import "./index.css";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+
+
+// [{"name":"Brainey","age":200,"height":"5cm","id":0}]
