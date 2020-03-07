7b02cb3cda792ca3c734a902c5ee184ab70bc8e4
Tyler Alsop <tylerjalsop@gmail.com>
2020-03-06 18:12:09 -0700

    Changed the App from a class based to functional.

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -7,20 +7,18 @@ import SmurfForm from './SmurfForm';
 import SmurfList from './SmurfList';
 
 
-class App extends Component {
-  render() {
-    return (
-      <SmurfContext.Provider value={{ }}>
-        <div className="App">
-          <h1>SMURFS! 2.0 W/ Redux</h1>
-          <div>Welcome to your state management version of Smurfs!</div>
-          <div>Start inside of your `src/index.js` file!</div>
-          <div>Have fun!</div>
-        </div>
-      </SmurfContext.Provider>
+function App () {
+  return (
+    <SmurfContext.Provider value={{ }}>
+      <div className="App">
+        <h1>SMURFS! 2.0 W/ Redux</h1>
+        <div>Welcome to your state management version of Smurfs!</div>
+        <div>Start inside of your `src/index.js` file!</div>
+        <div>Have fun!</div>
+      </div>
+    </SmurfContext.Provider>
 
-    );
-  }
-}
+  );
+};
 
 export default App;
